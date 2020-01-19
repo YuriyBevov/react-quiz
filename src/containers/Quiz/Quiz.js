ac5dacadcf35fc54b1339b596603dec99136f51d
Yuriy Bevov <jury19y@mail.ru>
2020-01-19 21:50:52 +0300

    изменение вопроса

--- src/containers/Quiz/Quiz.js
+++ src/containers/Quiz/Quiz.js
@@ -4,38 +4,38 @@ import ActiveQuiz from '../../components/ActiveQuiz/ActiveQuiz'
 
 class Quiz extends Component {
   state = {
+    activeQuestion: 0,
+
     quiz: [
       {
-
         question: 'Какого цвета небо ?',
-
+        rightAnswerId: 2,
+        id: 1,
         answers: [
-          {
-            text: 'Черный', id: 1
-          },
-          {
-            text: 'Синий', id: 2
-          },
-          {
-            text: 'Красный', id: 3
-          },
-          {
-            text: 'Зеленый', id: 4
-          },
-        ],
-
-        rightAnswerId: 2
-      }
+          {text: 'Черный', id: 1},
+          {text: 'Синий', id: 2},
+          {text: 'Красный', id: 3},
+          {text: 'Зеленый', id: 4},
+        ]
+      },
+      {
+        question: 'В каком году основан СПб ?',
+        rightAnswerId: 3,
+        id: 2,
+        answers: [
+          {text: '1700', id: 1},
+          {text: '1705', id: 2},
+          {text: '1703', id: 3},
+          {text: '1803', id: 4},
+        ]
+      },
     ]
   }
 
   onAnswerClickHandler = answerId => {
-    console.log(answerId)
-    if (answerId === this.state.quiz[0].rightAnswerId) {
-      console.log('Answer is true')
-    } else {
-      console.log('none')
-    }
+    this.setState({
+      activeQuestion: this.state.activeQuestion + 1
+    })
   }
 
   render() {
@@ -46,9 +46,11 @@ class Quiz extends Component {
           <h1>Ответьте на все вопросы</h1>
 
           <ActiveQuiz
-            answers={this.state.quiz[0].answers}
-            question={this.state.quiz[0].question}
+            answers={this.state.quiz[this.state.activeQuestion].answers}
+            question={this.state.quiz[this.state.activeQuestion].question}
             onAnswerClick = {this.onAnswerClickHandler}
+            quizLength={this.state.quiz.length}
+            answerNumber={this.state.activeQuestion + 1}
           />
         </div>
       </div>
