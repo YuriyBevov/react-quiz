42d9f9d1e42501bdbf7d4da6722dd4ea8df08e29
Yuriy Bevov <jury19y@mail.ru>
2020-02-06 15:35:24 +0300

    Загрузка теста

---
+++ src/axios/axios-quiz.js
@@ -0,0 +1,5 @@
+import axios from 'axios'
+
+export default axios.create({
+  baseURL: 'https://react-quiz-dd46a.firebaseio.com'
+})
--- src/containers/Quiz/Quiz.js
+++ src/containers/Quiz/Quiz.js
@@ -2,6 +2,8 @@ import React, {Component} from 'react'
 import classes from './Quiz.module.css'
 import ActiveQuiz from '../../components/ActiveQuiz/ActiveQuiz'
 import FinishedQuiz from '../../components/FinishedQuiz/FinishedQuiz'
+import axios from '../../axios/axios-quiz'
+import Loader from '../../components/UI/Loader/Loader'
 
 class Quiz extends Component {
   state = {
@@ -9,31 +11,8 @@ class Quiz extends Component {
     answerState: null,
     isFinished: false,
     results: {},
-
-    quiz: [
-      {
-        question: 'Какого цвета небо ?',
-        rightAnswerId: 2,
-        id: 1,
-        answers: [
-          {text: 'Черный', id: 1},
-          {text: 'Синий', id: 2},
-          {text: 'Красный', id: 3},
-          {text: 'Зеленый', id: 4},
-        ]
-      },
-      {
-        question: 'В каком году основан СПб ?',
-        rightAnswerId: 3,
-        id: 2,
-        answers: [
-          {text: '1700', id: 1},
-          {text: '1705', id: 2},
-          {text: '1703', id: 3},
-          {text: '1803', id: 4},
-        ]
-      },
-    ]
+    quiz: [],
+    loading: true
   }
 
   onAnswerClickHandler = answerId => {
@@ -95,6 +74,20 @@ class Quiz extends Component {
     })
   }
 
+  async componentDidMount() {
+    try {
+      const response = await axios.get(`quizes/${this.props.match.params.id}.json`)
+      const quiz = response.data
+
+      this.setState({
+        quiz,
+        loading: false
+      })
+    } catch(e) {
+      console.log(e)
+    }
+  }
+
   render() {
     return (
       <div className={classes.Quiz}>
@@ -103,22 +96,27 @@ class Quiz extends Component {
           <h1>Ответьте на все вопросы</h1>
 
           {
-            this.state.isFinished
-            ? <FinishedQuiz
-                results = {this.state.results}
-                quiz = {this.state.quiz}
-                onRetry={this.retryHandler}
-            />
+            this.state.loading
+            ? <Loader/ >
+            :
+                this.state.isFinished
+                ? <FinishedQuiz
+                    results = {this.state.results}
+                    quiz = {this.state.quiz}
+                    onRetry={this.retryHandler}
+                  />
+                : <ActiveQuiz
+                    answers={this.state.quiz[this.state.activeQuestion].answers}
+                    question={this.state.quiz[this.state.activeQuestion].question}
+                    onAnswerClick = {this.onAnswerClickHandler}
+                    quizLength={this.state.quiz.length}
+                    answerNumber={this.state.activeQuestion + 1}
+                    state={this.state.answerState}
+                  />
 
-            : <ActiveQuiz
-              answers={this.state.quiz[this.state.activeQuestion].answers}
-              question={this.state.quiz[this.state.activeQuestion].question}
-              onAnswerClick = {this.onAnswerClickHandler}
-              quizLength={this.state.quiz.length}
-              answerNumber={this.state.activeQuestion + 1}
-              state={this.state.answerState}
-            />
           }
+
+
 
 
         </div>
